f77ea3434b3ec6dd29c69e3780cee4e1a637fbbd
garrett <garrett.freibott@connexta.com>
2019-07-08 15:53:11 -0700

    temp

---
+++ gateway/src/main/java/com/connexta/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 Connexta, LLC
+ *
+ * Released under the GNU Lesser General Public License version 3; see
+ * https://www.gnu.org/licenses/lgpl-3.0.html
+ */
+package com.connexta.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
+}
